858b9bb0cf65ab2fd7e5877c8664530d014116b1
Novruz <novruztarverdiyev1@gmail.com>
2023-11-18 16:14:00 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIrstDiscordBotC_
+{
+    internal class Program
+    {
+        private static DiscordClient Client;
+        private static CommandsNextExtension Commands;
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
