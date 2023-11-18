06e259fcd212dfccf4c324c28fbfea1deb1c3e34
Novruz <novruztarverdiyev1@gmail.com>
2023-11-18 17:46:53 +0400

    Test Commands applied to understand CommandContext class.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using FIrstDiscordBotC_.Commands;
+using FIrstDiscordBotC_.Config;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FIrstDiscordBotC_
@@ -12,9 +18,50 @@ namespace FIrstDiscordBotC_
     {
         private static DiscordClient Client;
         private static CommandsNextExtension Commands;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            JsonReader jsonReader = new JsonReader();  
+            await jsonReader.ReadJsonAsync();
 
+            DiscordConfiguration discordConfiguration = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfiguration);
+
+            Client.UseInteractivity(new InteractivityConfiguration
+            {
+                Timeout = TimeSpan.FromMinutes(1)
+            });
+
+            Client.Ready += Client_Ready;
+
+            
+
+            CommandsNextConfiguration commandConfig= new CommandsNextConfiguration()
+            {
+                StringPrefixes=new string[] {jsonReader.Prefix},
+                EnableMentionPrefix = true,
+                EnableDms=true,
+                EnableDefaultHelp = false
+            };
+
+            Commands = Client.UseCommandsNext(commandConfig);
+
+            Commands.RegisterCommands<TestCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1); //To keep but running forever if project is running.
+
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
         }
     }
 }
